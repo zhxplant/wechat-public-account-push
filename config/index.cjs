8e6b9aabac216824174068bdcee2318d20ab7283
zhxplant <113841437+zhxplant@users.noreply.github.com>
2022-11-13 19:13:37 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -26,26 +26,26 @@ const USER_CONFIG = {
       useTemplateId: 'y54ZOGfee-e9WP5_gMxTEdFGkkYwZJYqtJjAUW022Do',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-06',
-//       festivals: [
-//         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-//         {
-//           type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-//         },
-//         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-//         {
-//           type: '生日', name: '李四', year: '1996', date: '09-31',
-//         },
-//         {
-//           type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-//         },
-//       ],
-//       // 我们在一起已经有xxxx天了的配置
-//       customizedDateList: [
-//         // 在一起的日子
-//         { keyword: 'love_day', date: '2022-09-08' },
-//         // 结婚纪念日
-//         { keyword: 'marry_day', date: '2022-09-09' },
-//       ],
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '李四', year: '1996', date: '09-31',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-09-08' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
+      ],
     },
   ],
 
