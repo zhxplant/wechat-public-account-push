b045349036d24f72fa470bcb053d29f6ac2154b1
zhxplant <113841437+zhxplant@users.noreply.github.com>
2022-11-13 17:19:33 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,9 +23,9 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oG50v5pD56-dg8Pih6QW3G7LljL0	',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '	y54ZOGfee-e9WP5_gMxTEdFGkkYwZJYqtJjAUW022Do',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '10-06',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
