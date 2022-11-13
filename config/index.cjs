6202442c183c55fb527c7ac99bf2ea130a16236f
zhxplant <113841437+zhxplant@users.noreply.github.com>
2022-11-13 20:40:53 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id:['oG50v5pD56-dg8Pih6QW3G7LljL0'],
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'mrC54Nfho1ZZYOlolpB-oo8ZAlCqU2DY6WHXUj5nJFo',
+      useTemplateId: 'v_-vcOHlZEB8QS5vP-1RFnpOfBpmcIH5zMeUyPJ4ItM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-06',
       festivals: [
