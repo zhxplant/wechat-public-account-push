1d71840bd542bd05f70922c01418cacc7223ad5d
zhxplant <113841437+zhxplant@users.noreply.github.com>
2022-11-15 10:31:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id:'oG50v5lkCIjUES9z86QqySmVrOQo',
+      id:'oG50v5pD56-dg8Pih6QW3G7LljL0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'v_-vcOHlZEB8QS5vP-1RFnpOfBpmcIH5zMeUyPJ4ItM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
